48610c93711885d808d32e75587e74d97ae1ee2d
U-DAVEPC2\Kevin <Kevin@davepc2.(none)>
2009-08-31 13:39:44 -0400

    before inserting ford

---
+++ source/lost/in/the/space/bridge/Ford.java
@@ -0,0 +1,69 @@
+package lost.in.the.space.bridge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+import org.zootella.cheat.process.Mistake;
+import org.zootella.cheat.state.Receive;
+
+public class Ford {
+	
+	//get access to it
+	public static Ford instance() {
+		if (ford == null)
+			ford = new Ford();
+		return ford;
+	}
+	private static Ford ford;
+
+	//make
+	public Ford() {
+		down = new ArrayList<JSONObject>();
+		up = new ArrayList<JSONObject>();
+		receive = new ArrayList<Receive>();
+	}
+	private final List<JSONObject> down;
+	private final List<JSONObject> up;
+	private final List<Receive> receive;
+
+	//send a message down or up
+	public void sendDown(JSONObject o) {
+		down.add(o);
+		arrived();
+	}
+	public void sendUp(JSONObject o) {
+		up.add(o);
+		arrived();
+	}
+
+	//get a message that went down or up
+	public JSONObject receiveDown() {
+		if (down.isEmpty()) return null;
+		return down.remove(0);
+	}
+	public JSONObject receiveUp() {
+		if (up.isEmpty()) return null;
+		return up.remove(0);
+	}
+	
+	//sign up so you get called when new messages arrive
+	public void subscribe(Receive r) {
+		receive.add(r);
+	}
+	private void arrived() {
+		try {
+			for (Receive r : receive) {
+				r.receive();
+			}
+		} catch (Exception e) { Mistake.stop(e); }
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
